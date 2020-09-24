dca035ce6c3d764c94d1d4c64e7f9d9b2e434310
Crossle Song <crosslesong@gmail.com>
2020-09-24 16:28:42 +0800

    Increase rpc timeout

--- mixin/rpc.go
+++ mixin/rpc.go
@@ -79,7 +79,7 @@ type NodeInfo struct {
 
 func NewMixinNetwork(node string) *MixinNetwork {
 	return &MixinNetwork{
-		httpClient: &http.Client{Timeout: 10 * time.Second},
+		httpClient: &http.Client{Timeout: 20 * time.Second},
 		node:       node,
 	}
 }
